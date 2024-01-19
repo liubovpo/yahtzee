06ba580570da2649eccdb7d0b521fa41d7263a53
liubovpo <lyubov.html@gmail.com>
2024-01-19 19:46:27 +0100

    count

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import {
   Container,
@@ -12,36 +12,41 @@ import {
   Button,
   Paper,
   Typography,
-} from '@mui/material';
-import InputAdornment from '@mui/material/InputAdornment';
+} from "@mui/material";
+import InputAdornment from "@mui/material/InputAdornment";
 
 const categories = [
-  'Ones',
-  'Twos',
-  'Threes',
-  'Fours',
-  'Fives',
-  'Sixes',
-  '3 of a Kind',
-  '4 of a Kind',
-  'Full House',
-  'Small Straight',
-  'Large Straight',
-  'Yahtzee',
-  'Chance',
+  "Ones",
+  "Twos",
+  "Threes",
+  "Fours",
+  "Fives",
+  "Sixes",
+  "3 of a Kind",
+  "4 of a Kind",
+  "Full House",
+  "Small Straight",
+  "Large Straight",
+  "Yahtzee",
+  "Chance",
 ];
 
-const fixedPointCategories = ['Full House', 'Large Straight', 'Small Straight', 'Yahtzee'];
+const fixedPointCategories = [
+  "Full House",
+  "Large Straight",
+  "Small Straight",
+  "Yahtzee",
+];
 
 const fixedPoints = {
-  'Full House': 25,
-  'Large Straight': 40,
-  'Small Straight': 30,
-  'Yahtzee': 50,
+  "Full House": 25,
+  "Large Straight": 40,
+  "Small Straight": 30,
+  Yahtzee: 50,
 };
 
 const GamePage = () => {
-  const [players, setPlayers] = useState([]); // Add more players as needed
+  const [players, setPlayers] = useState([]);
   const [scores, setScores] = useState({});
   const [confirmedScores, setConfirmedScores] = useState({});
   const { id } = useParams();
@@ -55,18 +60,18 @@ const GamePage = () => {
         return response.json();
       })
       .then((data) => {
-        setPlayers(data.map(player=>player.name));
+        setPlayers(data.map((player) => player.name));
       })
       .catch((error) => console.log(error));
   }, []);
 
   const buttonStyle = {
-    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-    color: '#ffffff',
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    color: "#ffffff",
   };
 
   const handleScoreConfirmation = (category, playerName, score) => {
-    if (score !== '') {
+    if (score !== "") {
       setScores((prevScores) => ({
         ...prevScores,
         [playerName]: { ...(prevScores[playerName] || {}), [category]: score },
@@ -74,33 +79,65 @@ const GamePage = () => {
 
       setConfirmedScores({
         ...confirmedScores,
-        [playerName]: { ...(confirmedScores[playerName] || {}), [category]: true },
+        [playerName]: {
+          ...(confirmedScores[playerName] || {}),
+          [category]: true,
+        },
       });
     }
     console.log(scores);
   };
 
-  return (
-    <Container sx={{mt:10}}>
+  const handleScoreCount = () => {
+    const totalScores = players.map((player) => {
+      return categories.reduce(
+        (total, category) => total + Number(scores[player]?.[category] || 0),
+        0
+      );
+    });
 
-      <TableContainer component={Paper} sx={{ maxHeight: "calc(100vh - 100px)"}}>
+    return totalScores;
+  };
+
+  const areAllCategoriesConfirmed = (player) => {
+    return categories.every((category) => confirmedScores[player]?.[category]);
+  };
+
+  return (
+    <Container sx={{ mt: 10 }}>
+      <TableContainer
+        component={Paper}
+        sx={{ maxHeight: "calc(100vh - 200px)" }}
+      >
         <Table stickyHeader aria-label="sticky table">
           <TableHead>
             <TableRow>
-              <TableCell>Categories</TableCell>
+              <TableCell sx={{ fontWeight: "bold" }}>Categories</TableCell>
               {players.map((player, playerIndex) => (
-                <TableCell key={playerIndex} align="center">
+                <TableCell
+                  sx={{ fontWeight: "bold" }}
+                  key={playerIndex}
+                  align="center"
+                >
                   {player}
                 </TableCell>
               ))}
             </TableRow>
-            <TableRow >
-              <TableCell sx={{fontWeight:'bold'}}>Total</TableCell>
+            <TableRow>
+              {/* <TableCell sx={{ fontWeight: "bold" }}>Total</TableCell>
               {players.map((player, playerIndex) => (
-                <TableCell sx={{fontWeight:'bold'}} key={playerIndex}>
-                  {categories.reduce((total, category) => total + (scores[player]?.[category] || 0), 0)}
+                <TableCell
+                  sx={{ fontWeight: "bold" }}
+                  key={playerIndex}
+                  align="center"
+                >
+                  {categories.reduce(
+                    (total, category) =>
+                      total + (scores[player]?.[category] || 0),
+                    0
+                  )}
                 </TableCell>
-              ))}
+              ))} */}
             </TableRow>
           </TableHead>
           <TableBody>
@@ -108,49 +145,84 @@ const GamePage = () => {
               <TableRow key={categoryIndex}>
                 <TableCell>{category}</TableCell>
                 {players.map((player, playerIndex) => {
-                  const isFixedCategory = fixedPointCategories.includes(category);
+                  const isFixedCategory =
+                    fixedPointCategories.includes(category);
                   const isScoreConfirmed =
-                    confirmedScores[player] && confirmedScores[player][category];
+                    confirmedScores[player] &&
+                    confirmedScores[player][category];
 
-                  const inputValue = scores[player]?.[category] || '';
+                  const inputValue = scores[player]?.[category] || "";
 
                   return (
                     <TableCell key={playerIndex}>
                       {isFixedCategory ? (
-                        <Button
-                          variant="contained"
-                          // style={buttonStyle}
-                          disabled={isScoreConfirmed}
-                          onClick={() => handleScoreConfirmation(category, player, fixedPoints[category])}
-                        >
-                          Got it ({fixedPoints[category]} pt)
-                        </Button>
+                        <>
+                          <Button
+                            variant="contained"
+                            disabled={isScoreConfirmed}
+                            onClick={() =>
+                              handleScoreConfirmation(
+                                category,
+                                player,
+                                fixedPoints[category]
+                              )
+                            }
+                          >
+                            Got it ({fixedPoints[category]} pt)
+                          </Button>
+                          <Button
+                            sx={{ ml: 2 }}
+                            variant="contained"
+                            disabled={isScoreConfirmed}
+                            onClick={() =>
+                              handleScoreConfirmation(category, player, 0)
+                            }
+                          >
+                            0 pt
+                          </Button>
+                        </>
                       ) : (
                         <>
                           {isScoreConfirmed ? (
-                            <Typography variant="body1">{scores[player]?.[category]}</Typography>
+                            <Typography variant="body1">
+                              {scores[player]?.[category]}
+                            </Typography>
                           ) : (
                             <>
                               <TextField
                                 id="filled-start-adornment"
                                 InputProps={{
-                                  endAdornment: <InputAdornment position="end">pt</InputAdornment>,
+                                  endAdornment: (
+                                    <InputAdornment position="end">
+                                      pt
+                                    </InputAdornment>
+                                  ),
                                 }}
                                 variant="outlined"
                                 value={inputValue}
-                                style={{ marginRight: '10px' }}
+                                type="number"
+                                style={{ marginRight: "10px" }}
                                 onChange={(e) => {
-                                  const value = parseInt(e.target.value, 10) || 0;
+                                  const value = e.target.value;
                                   setScores((prevScores) => ({
                                     ...prevScores,
-                                    [player]: { ...(prevScores[player] || {}), [category]: value },
+                                    [player]: {
+                                      ...(prevScores[player] || {}),
+                                      [category]: value,
+                                    },
                                   }));
                                 }}
                               />
                               <Button
                                 variant="contained"
                                 style={buttonStyle}
-                                onClick={() => handleScoreConfirmation(category, player, inputValue)}
+                                onClick={() =>
+                                  handleScoreConfirmation(
+                                    category,
+                                    player,
+                                    inputValue
+                                  )
+                                }
                               >
                                 Confirm
                               </Button>
@@ -166,6 +238,14 @@ const GamePage = () => {
           </TableBody>
         </Table>
       </TableContainer>
+      <Button
+        disabled={!players.every((player) => areAllCategoriesConfirmed(player))}
+        variant="contained"
+        sx={{ mt: 2 }}
+        onClick={() => console.log(handleScoreCount())}
+      >
+        Count
+      </Button>
     </Container>
   );
 };
